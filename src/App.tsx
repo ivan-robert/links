399437567d051cd5165c817232cda2805e8966b1
Ivan Robert <ivan.rog.robert@gmail.com>
2025-09-22 14:13:33 +0200

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router';
 import HomePage from './components/HomePage';
 import LinksPage from './components/LinksPage';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100">
+      <Analytics debug={false} /> 
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/links" element={<LinksPage />} />
